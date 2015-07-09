2fa88285d3dec5b5dcf6440ebb528f56c0b4343a
Kristin Houchin <kmhouchin@gmail.com>
2015-07-09 11:05:05 -0500

    Got mobile nav to work. Dry too.

--- mobile_nav/public/global.js
+++ mobile_nav/public/global.js
@@ -1,18 +1,9 @@
-// var mobile_menu document.getElementById('menu-icon')
-//
-// mobile_menu.onclick =
-//
-// function showStuff(id) {
-  //   mobile_menu.style.display = 'block';
-  // }
-
+function menuCreate(){
+  var list = document.getElementsByClassName("menu")[0];
+  list.classList.toggle("show");
   
-  var menu_click = function (){
+}
 
-    var list_menu = document.getElementsByClassName("menu")[0];
-      list_menu.classList.toggle("show");
-  }
+var menu_button_click = document.getElementsByClassName("menu_button")[0];
 
-  var menu_button_click = document.getElementsByClassName("menu_button");
-
-  menu_button_click.onclick = menu_click;
+menu_button_click.onclick = menuCreate;
